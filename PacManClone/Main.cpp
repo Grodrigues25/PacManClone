c2f9c84067cc35f577f8c4289c1798b1467e922c
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2024-06-23 01:29:15 +0100

    Researching on map specs Writing the map in ASCII for guidance in a comment

--- PacManClone/Main.cpp
+++ PacManClone/Main.cpp
@@ -11,6 +11,52 @@ using namespace std;
 // --------------------------------------------
 // TODO: Investigate the best way to store the board format and to then render it
 
+/*
+The mazes are built carefully to closely match design patterns deduced from the original maps found in Pac-Man and Ms. Pac-Man:
+
+Map is 28x31 tiles.
+Paths are only 1 tile thick
+No sharp turns (i.e. intersections are separated by atleast 2 tiles).
+There are 1 or 2 tunnels
+No dead-ends.
+Only I, L, T, or + wall shapes are allowed, including the occasional rectangular wall.
+Any non-rectangular wall pieces must only be 2 tiles thick.
+
+*/
+
+/*
+############################
+#............##............#
+#.####.#####.##.#####.####.#
+#.####.#####.##.#####.####.#
+#.####.#####.##.#####.####.#
+#..........................#
+#.####.##.########.##.####.#
+#.####.##.########.##.####.#
+#......##....##....##......#
+######.#####.##.#####.######
+     #.#####.##.#####.#
+     #.##..........##.#
+     #.##.###--###.##.#
+######.##.#      #.##.######
+..........#      #..........
+######.##.#      #.##.######
+     #.##.########.##.#     
+     #.##..........##.#     
+     #.##.########.##.#     
+######.##.########.##.######
+#............##............#
+#.####.#####.##.#####.####.#
+#.####.#####.##.#####.####.#
+#...##................##...#
+###.##.##.########.##.##.###
+###.##.##.########.##.##.###
+#......##....##....##......#
+#.##########.##.##########.#
+#.##########.##.##########.#
+#..........................#
+############################
+*/
 
 void renderBoard(sf::RenderWindow& window) {
 
