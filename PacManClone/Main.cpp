1a89a4aa1aea1fe999d1da7e030910f2b679157f
Gonçalo Rodrigues <gmgrenhorodrigues@gmail.com>
2024-06-26 23:31:13 +0100

    Continued working on rendering map. Was succesful in rendering map for the first time. Now need to scale the map to the window size.
    Worked on some images of the map to be used by the rendering as a tilemap.

--- PacManClone/Main.cpp
+++ PacManClone/Main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 
 // TODO Board
 // --------------------------------------------
-// TODO: Investigate the best way to store the board format and to then render it -> This is the solution: https://www.sfml-dev.org/tutorials/2.6/graphics-vertex-array.php
+// TODO: Investigate the best way to store the board format and to then render it -> This is the solution: https://www.sfml-dev.org/tutorials/2.6/graphics-vertex-array.php -> DONE
+// TODO: Scale Map file to proper window size
 
 /*
 The mazes are built carefully to closely match design patterns deduced from the original maps found in Pac-Man and Ms. Pac-Man:
@@ -61,21 +62,19 @@ Any non-rectangular wall pieces must only be 2 tiles thick.
 
 void renderBoard(sf::RenderWindow& window) {
 
+    //for (int i = 0; i < 180; i++)
+    //{
+    //    cout << i << ", ";
+    //}
+
     const int level[] =
     {
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167,
     };
 
     // create the tilemap from the level definition
     TileMap map;
-    if (!map.load("C:\\Users\\gonca\\source\\repos\\PacManClone\\assets\\Pac Man tile set.png", sf::Vector2u(16, 16), level, 16, 8))
+    if (!map.load("C:\\Users\\gonca\\source\\repos\\PacManClone\\assets\\Map.png", sf::Vector2u(16, 16), level, 14, 12))
         cout << "Error" << endl;
 
     window.draw(map);
@@ -86,7 +85,7 @@ int main() {
 
     // SETTINGS
     int windowWidth = 800;
-    int windowHeight = 950;
+    int windowHeight = 960;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
